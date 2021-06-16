f579cc0d762ebed256c9e5b7f619d3da75e6bce7
Rahul Kumar Singh <rahulkrsingh32@gmail.com>
2021-06-16 22:02:14 +0530

    updated project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -24,14 +24,22 @@ export const projects = [
     tags: ['React', 'CSS', 'Bootstrap'],
     source: 'https://github.com/Rahulkrsingh32/website.git',
     visit: 'https://agrofarm.netlify.app/',
-    id: 1,
+    id: 2,
+  },
+  {
+    title: 'Demo Website with Animation',
+    description: "It is the Basic Demo website built with React, CSS, Bootstrap. this website has tons of amazing animations which makes it look facinating. This website can be used by anyone by just making some changes to its source code. Do check it out",
+    image: '/images/4.png',
+    tags: ['React', 'CSS', 'Bootstrap'],
+    source: 'https://github.com/Rahulkrsingh32/coolwebsite.git',
+    visit: 'https://coolwebsite.netlify.app/',
+    id: 3,
   },
 ];
 
 export const TimeLineData = [
   { year: 2016, text: 'Passed Std X from CBSE', },
-  { year: 2018, text: 'Passed Std XII (CBSE)', },
-  { year: 2018, text: 'Took Admission in ITER, SOA University, Bhubneswar to persue B.tech in CS', },
+  { year: 2018, text: 'Passed Std XII (CBSE) and Took Admission in ITER, SOA University, Bhubneswar to persue B.tech in CS', },
   { year: 2019, text: 'First case of COVID-19 found, and also I Found my interst in web-Development and started learning it', },
   { year: 2020, text: 'Done internship, that helped me in my coading skills', },
   { year: 2021, text: 'Ongoing... ', }
